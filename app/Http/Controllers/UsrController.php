9666dd3b6b99ae112fe2aed30149839d40300e77
jpvasquez <vasquezjp14@gmail.com>
2018-01-02 22:41:38 +0800

    updated personal information

--- app/Http/Controllers/UsrController.php
+++ app/Http/Controllers/UsrController.php
@@ -335,6 +335,7 @@ class UsrController extends Controller
                         'nationality' => $usr['nationality'],
                         'objectives'   => $usr['objectives']
                     ]);
+                $this->msg['success']['prsnl'] = 'Successfully Updated';
             else:
                 DB::table('personal_information')->insert([
                     'genid' => Auth::user()->genid,
@@ -347,6 +348,7 @@ class UsrController extends Controller
                     'nationality'   => $usr['nationality'],
                     'objectives'     => $usr['objectives']
                 ]);
+                $this->msg['success']['prsnl'] = 'Successfully Added';
             endif;
         endif;
         print_r(json_encode($this->msg, JSON_PRETTY_PRINT));
