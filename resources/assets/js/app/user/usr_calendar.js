7b1e61b7d0f4566e328a3a7b45506b5c452890c4
jvasquez <jvasquez@stratpoint.com>
2018-01-19 16:48:35 +0800

    updated admin schedule

--- resources/assets/js/app/user/usr_calendar.js
+++ resources/assets/js/app/user/usr_calendar.js
@@ -1,8 +1,8 @@
 'use strict'; 
 var usrContent = angular.module('usrContent', ['ui.calendar', 'ui.bootstrap']);
 
-usrContent.controller('ctrlCalendar', ['$scope', '$rootScope', '$timeout', '$http', '$compile', 'uibDateParser', 'uiCalendarConfig',
-	function($scope, $rootScope, $timeout, $http, $compile, uibDateParser, uiCalendarConfig) {
+usrContent.controller('ctrlCalendar', ['$scope', '$rootScope', '$timeout', '$http', '$compile', '$filter', 'uibDateParser', 'uiCalendarConfig',
+	function($scope, $rootScope, $timeout, $http, $compile, $filter, uibDateParser, uiCalendarConfig) {
 
     $scope.open_calendar = function($event, index, datepicker){
         $scope[datepicker] = {}; $scope[datepicker].open = {};
@@ -14,9 +14,40 @@ usrContent.controller('ctrlCalendar', ['$scope', '$rootScope', '$timeout', '$htt
         showWeeks: false,
         minDate: new Date()
     };
-    $scope.NoWeeks = {
-        showWeeks: false
+    $scope.MaxDateTo = {
+        showWeeks: false,
+        minDate: new Date()
     };
+    $scope.MaxDateCheckout = {
+        showWeeks: false,
+        minDate: new Date()
+    };
+    $scope.getMax = function(nwdate, num){
+        switch(num){
+            case 0:
+                $scope.MaxDateCheckout = {
+                    showWeeks: false,
+                    minDate: nwdate
+                }
+                break;
+            case 1:
+                $scope.MaxDateTo = {
+                    showWeeks: false,
+                    minDate: nwdate
+                }
+                break;
+        }
+    }
+    
+    $scope.activities = [
+        {id: 1, name: 'Reserved', color: '#FFF', background: '#17b13c'},
+        {id: 2, name: 'Out of Service', color: '#FFF', background: '#a6a6a6'},
+    ]
+
+    // $scope.MaxDate = {
+    //     showWeeks: false,
+    //     maxDate: new Date()
+    // };
     
     /* event source that contains custom events on the scope */
     $scope.guest_scheds = [
@@ -45,21 +76,20 @@ usrContent.controller('ctrlCalendar', ['$scope', '$rootScope', '$timeout', '$htt
       console.log($scope.guest_scheds);
     };
 
-    $scope.AdminSched = function(schd, clr, bg) {
-        var bg = bg ? bg : '#6dc5e6',
-            clr = clr ? clr : '#FFF',
+    $scope.AdminSched = function(schd) {
+        var activities = $filter('filter')($scope.activities, {id: schd.activity})[0],
             admn_sdate = moment(schd.fromdate).startOf('day'),
             admn_edate = moment(schd.todate).startOf('day');
-        // console.log(todayDate);
       $scope.admin_scheds.push({
-        // title: 'Open Sesame',
-        color: clr,
-        background: bg,
+        title: schd.ttl,
+        color: activities.background,
+        textColor: activities.color,
         start: admn_sdate.clone().format('YYYY-MM-DD'),
         end: admn_edate.clone().add(1, 'day').format('YYYY-MM-DD'),
         className: ['admin'],
         currentTimezone: 'Asia/Manila' // an option!
       });
+      $scope.schd = {};
       console.log($scope.admin_scheds);
     };
 
@@ -75,28 +105,28 @@ usrContent.controller('ctrlCalendar', ['$scope', '$rootScope', '$timeout', '$htt
           right: 'month,agendaWeek,agendaDay,listWeek'
         },
         navLinks: true, // can click day/week names to navigate views
-        selectable: true,
+        // selectable: true,
         selectHelper: true,
-        select: function(start, end) {
-            var title = prompt('Event Title:');
-            var eventData;
-            if (title) {
-              // eventData = {
-              //   title: title,
-              //   start: start,
-              //   end: end
-              // };
-              $scope.admin_scheds.push({
-                title: title,
-                start: start.clone().format('YYYY-MM-DD'),
-                end: end.clone().format('YYYY-MM-DD'),
-                className: ['admin'],
-                currentTimezone: 'Asia/Manila' // an option!
-              });
-              $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-            }
-            $('#calendar').fullCalendar('unselect');
-        }
+        // select: function(start, end) {
+        //     var title = prompt('Event Title:');
+        //     var eventData;
+        //     if (title) {
+        //       // eventData = {
+        //       //   title: title,
+        //       //   start: start,
+        //       //   end: end
+        //       // };
+        //       $scope.admin_scheds.push({
+        //         title: title,
+        //         start: start.clone().format('YYYY-MM-DD'),
+        //         end: end.clone().format('YYYY-MM-DD'),
+        //         className: ['admin'],
+        //         currentTimezone: 'Asia/Manila' // an option!
+        //       });
+        //       $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+        //     }
+        //     $('#calendar').fullCalendar('unselect');
+        // }
       }
     };
     /* event sources array*/
